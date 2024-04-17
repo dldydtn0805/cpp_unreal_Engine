78fe60c48b4ae6f3d012d76690a18eba17423112
dldydtn0805 <ssafysoo95@gmail.com>
2024-04-17 15:13:19 +0900

    init Comit

---
+++ TestUnrealEngine/Source/TestUnrealEngine/TestUnrealEngine.cpp
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestUnrealEngine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestUnrealEngine, "TestUnrealEngine" );
+
+/*
+
+환경 설정
+
+본 강의는 4.26.2로 구성
+
+영어 권장
+
+프로젝트 세팅
+
+c++ / 블루프린트 중 하나를 선택해도 언제든지 나머지로 넘어갈 수 있다
+
+컨텐츠 브라우저
+
+사용 가능 에셋 표기
+
+
+*/
